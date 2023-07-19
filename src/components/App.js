4f383bbafb8fd6a98597077bc4a1ef9ea129dc46
Krishna Rathod <88035445+KryshnaRathod@users.noreply.github.com>
2023-07-19 14:35:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,20 @@
 import React, { useState } from 'react'
 import '../styles/App.css';
-import { Loader } from './Loader';
-import { PhotoFrame } from './PhotoFrame';
+
 const App = () => {
-    const [state, setState] = useState({
-        title: '',
-        url:'',
-        loaderDisplay: false,
-    });
-    
-    const inputHandler =(e)=>{
-        let value = e.target.value;
-        // if(!value) return;
-        setState({
-            ...state,
-            loaderDisplay: true,
-        })
-        fetch('https://jsonplaceholder.typicode.com/photos/'+value)
-        .then((response)=>{
-            return response.json();
-        })
-        .then((data)=>{
-            // console.log(data);
-            setState({
-                title: data.title,
-                url: data.url,
-                loaderDisplay: false,
-            })
-        })
-        .catch((error)=> console.log(error))
-    }
+  const[size, setSize] = useState({
+    height:320,
+    width:320
+  });
+  // const[weight, setWeight] = useState(320);
+  const handleChange = (e)=>{
+    setSize({...size, [e.target.name]:e.target.value});
+  }
   return (
-    <div className='App'>
-        <label htmlFor='id-number'>Id number </label>
-        <input onChange={inputHandler} id='id-number' type={'number'} min={1} max={5000} />
-        {state.loaderDisplay ? <Loader /> :
-        state.title ? <PhotoFrame url={state.url} title={state.title} /> : console.log(state.title)}
+    <div id="main">
+      <input type="range" name={"height"} min={100} max={800} value={size.height} id={"height-slider"} onChange={handleChange}/>
+      <input type="range" name='width' min={100} max={800} value={size.width} id={"width-slider"} onChange={handleChange}/><br/>
+      <img src="src\image.jpeg" alt="image" id='resizable-img' height={size.height+"px"} width={size.width+"px"}/>
     </div>
   )
 }
